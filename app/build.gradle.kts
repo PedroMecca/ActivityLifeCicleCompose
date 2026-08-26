68e787e1e4e2c28f2b82eda193ccf33d48bc0561
pedro.bonelli <pebonellimecca@gmail.com>
2026-08-26 19:45:41 -0300

    feat: rememberSaveable - salvando dados pós quebra de tela

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.edu.ifsp.scl.prdm.sc303500x.activitylifeciclecompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.edu.ifsp.scl.prdm.sc303500x.activitylifeciclecompose"
--- app/src/main/java/br/edu/ifsp/scl/prdm/sc303500x/activitylifeciclecompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc303500x/activitylifeciclecompose/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.prdm.sc303500x.activitylifeciclecompose.ui.theme.ActivityLifeCicleComposeTheme
 
@@ -21,7 +31,6 @@ class MainActivity : ComponentActivity() {
             ActivityLifeCicleComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainScreen(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    var name by rememberSaveable { mutableStateOf ("") } //saveable mantém estado do valor
+                                                               // após quebra da tela
+    var age = rememberSaveable { mutableStateOf("") }
+    Column (
+        modifier = modifier.fillMaxSize()
+    ) {
+        TextField(
+            value = name,
+            label = { Text("Name") },
+            keyboardOptions =  KeyboardOptions( keyboardType =  KeyboardType.Text),
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {name = it} //passa o conteudo da caixa de texto pro nome
+        )
+        TextField(
+            value = age.value,
+            label = { Text("Age") },
+            keyboardOptions =  KeyboardOptions( keyboardType =  KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {age.value = it}
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     ActivityLifeCicleComposeTheme {
-        MainScreen("Android")
+        MainScreen()
     }
 }
