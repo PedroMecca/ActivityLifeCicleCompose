71164b99e5d28ded67fe60ec3a475a9bd97d973c
pedro.bonelli <pebonellimecca@gmail.com>
2026-08-26 19:45:11 -0300

    initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc303500x/activitylifeciclecompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.edu.ifsp.scl.prdm.sc303500x.activitylifeciclecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifsp.scl.prdm.sc303500x.activitylifeciclecompose.ui.theme.ActivityLifeCicleComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ActivityLifeCicleComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActivityLifeCicleComposeTheme {
+        MainScreen("Android")
+    }
+}
